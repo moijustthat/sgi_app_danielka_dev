828a49c5f0c227b661f2cd11799f9217d3eb8d44
moijustthat <moipaburto@gmail.com>
2024-05-15 18:32:23 -0600

    Formulario agregar productos

--- sgi_app_front/src/components/Pages/Inventario/Productos/AddProducto.jsx
+++ sgi_app_front/src/components/Pages/Inventario/Productos/AddProducto.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Banner from '../../../Common/Banner/Banner'
 import { Button, Grid } from '@mui/material'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { IconButton }  from '@mui/material';
 
 import './AddProducto.css'
 
@@ -8,7 +10,57 @@ const AddProducto = ({setOpen}) => {
   return (
     <div className='container'>
       <div className='glass'>
-        <button onClick={() => setOpen(false)}>Cancelar</button>
+       
+        <div className='formProducto'>
+          <div className='exit'>
+              <IconButton  onClick={() => setOpen(false)}>
+                  <ArrowBackIcon />
+              </IconButton>
+            </div>
+
+          <div className='form'>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+            <h1>Formulario con scroll</h1>
+          </div>
+
+          <div className='btnAgregarProducto'>
+            <h3>Agregar a la lista</h3>
+          </div>
+
+
+        </div>
+
+        <div className='nuevosProductos'>
+          <div className='listaNuevosProductos'>
+            <h3>Nuevo Producto con scroll obvio</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+            <h3>Nuevo Producto</h3>
+          </div>
+
+          <div className='btnAgregarLista'>
+            <h3>Agregar nuevos prodctos a la BD</h3>
+          </div>
+        </div>
       </div>
     </div>
 
